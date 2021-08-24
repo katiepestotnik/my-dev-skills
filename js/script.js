36fecb132eaeb0a08c49d05b6fd2b4869e78c434
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-08-24 12:56:59 -0600

    Fix append issue

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $('ul').on('click', 'span',handleDeleteSkill)
 init();
 function init() {
     //as soon as application loads, assign skills to empty array
-    skills = [];
+    skills= '';
     render();
 };
 
@@ -25,16 +25,17 @@ function init() {
 function handleAddSkill() {
     //store the value from input tag inside local variable
     const skill = $input.val();
+    console.log(skill)
     //Check for skill data
     if (skill) {
         //create the userinterface(ui) li for skill
         const $skill = $(`<li><span> X </span>${skill}</li>`)
         //push skill ui into skills list
-        skills.push($skill);
+        $('ul').append($skill);
         //clear input value //empty quote method overloading 
         $input.val('');
-        // call render() 
-       render();
+        skills = $('ul').html()
+        console.log(skills)
     } else {
         //also can do } else return;
         alert('No skill entered! Reenter a skill.')
@@ -44,6 +45,9 @@ function handleAddSkill() {
 function handleDeleteSkill() {
     console.log($(this))
     $(this).closest('li').remove();
+    skills = $('ul').html()
+    console.log(skills)
+
 
 };
 
@@ -53,5 +57,5 @@ function render() {
     // /!skills.length means empty array
     if (!skills.length) $ul.css('margin-bottom', '30px')
     else $ul.css('margin-bottom', '0px' )
-    $ul.append(skills)
+    $ul.html(skills)
 };
